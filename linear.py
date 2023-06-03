2f7a4657dabb40befdf43719a4ff745ebc557559
anhle32 <90089985+anhle32@users.noreply.github.com>
2023-06-03 10:19:45 +0700

    linear.py

--- linear.py
+++ linear.py
@@ -84,9 +84,21 @@ elif choice == 'Sử dụng mô hình để dự báo':
             # st.write(lines.columns)
             flag = True       
     if type=="Input":        
-        email = st.text_area(label="Input your content:")
+        giatri = st.number_input('Insert a number')
+        DT = st.number_input('Insert a number')
+        TN = st.number_input('Insert a number')
+        SPT = st.number_input('Insert a number')
+        GTC = st.number_input('Insert a number')
+        GD = st.number_input('Insert a number')
+        TCH = st.number_input('Insert a number')
+        GT = st.number_input('Insert a number')
+        DV = st.number_input('Insert a number')
+        VPCT = st.number_input('Insert a number')
+        LS = st.number_input('Insert a number')
+        
+        email={'giatri':giatri,	'DT':DT, 'TN':TN, 'SPT':SPT,	'GTC':GTC,	'GD':GD,	'TCH':TCH,	'GT':GT,	'DV':DV,	'VPCT':VPCT,	'LS':LS}
         if email!="":
-            lines = np.array([email])
+            lines = pd.DataFrame(email)
             flag = True
     
     if flag:
